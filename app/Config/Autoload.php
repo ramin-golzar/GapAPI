3cbe78360bf907be31cbd1cf6435eaaa6cada89f
Eraj <eraj@email.ir>
2023-11-12 19:30:00 +0330

    edit a namespace from Autoload.php

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -45,6 +45,7 @@ class Autoload extends AutoloadConfig
         'Config' => APPPATH . 'Config' ,
         'Libraries' => APPPATH . 'Libraries' ,
         'Libraries\GapAPI' => APPPATH . 'Libraries/GapAPI' ,
+        'Libraries\GapAPI\Messages' => APPPATH . 'Libraries/GapAPI/Messages' ,
     ];
 
     /**
