db9b9ec0ea006ff11b25e810968f79566dca9e25
Eraj <eraj@email.ir>
2023-11-12 17:24:01 +0330

    Create the URLs.php class for manage bot urls

---
+++ app/Libraries/GapAPI/URLs.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Libraries\GapAPI;
+
+class URLs
+{
+
+    /**
+     * This is a address for send message to gap server
+     */
+    public const SEND_ADDRESS = 'https://api.gap.im/sendMessage/';
+
+}
