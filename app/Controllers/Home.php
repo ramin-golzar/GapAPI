db2893eb5b27e002cef00145e73fde5d5434545f
Eraj <eraj@email.ir>
2023-11-12 16:21:56 +0330

    Test connection is done

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -17,14 +17,14 @@ class Home extends BaseController
     private function send (string $chatId): void {
         $client = \Config\Services::curlrequest ();
 
-        $url = 'https://api.gap.im/sendMessage';
+        $url = 'https://api.gap.im/sendMessage/';
 
         $option = [
             'headers' => [
                 'token' => '18b34dbfab054137d021173fbcc12fc0ee01bca35c8a2d52b566585b1ff71496' ,
             ] ,
             'form_params' => [
-                'char_id' => $chatId ,
+                'chat_id' => $chatId ,
                 'type' => 'text' ,
                 'data' => 'Hello, Welcome to my robot' ,
             ] ,
