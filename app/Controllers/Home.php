6c58db8100055d1b215245029da12d1b86ad62c2
Eraj <eraj@email.ir>
2023-11-12 15:56:26 +0330

    I hame written the code to test the connection

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,11 +1,36 @@
 <?php
-
 namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index()
-    {
-        return view('welcome_message');
+
+    public function index () {
+
+
+        $post = $this->request->getPost ();
+
+        if ($post ['type'] == 'join') {
+            $this->send ($post ['chat_id']);
+        }
     }
+
+    private function send (string $chatId): void {
+        $client = \Config\Services::curlrequest ();
+
+        $url = 'https://api.gap.im/sendMessage';
+
+        $option = [
+            'headers' => [
+                'token' => '18b34dbfab054137d021173fbcc12fc0ee01bca35c8a2d52b566585b1ff71496' ,
+            ] ,
+            'form_params' => [
+                'char_id' => $chatId ,
+                'type' => 'text' ,
+                'data' => 'Hello, Welcome to my robot' ,
+            ] ,
+        ];
+
+        $response = $client->request ('POST' , $url , $option);
+    }
+
 }
