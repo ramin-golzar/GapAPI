2b37d8aedb1bb5372420869f03b6dc7ed280d241
Eraj <eraj@email.ir>
2023-11-13 17:31:00 +0330

    initialize the properties of SendParams.php & writing some comments

--- app/Libraries/GapAPI/SendParams.php
+++ app/Libraries/GapAPI/SendParams.php
@@ -4,11 +4,46 @@ namespace App\Libraries\GapAPI;
 class SendParams
 {
 
+    /**
+     * Holds the user chat id
+     *
+     * @var string
+     */
     public string $chatId = null;
+
+    /**
+     * Holds the data type
+     *
+     * @var string
+     */
     public string $type = null;
+
+    /**
+     * Holds the date for send to gap server
+     *
+     * @var string
+     */
     public string $data = null;
+
+    /**
+     * Holds the json of reply keyboard
+     *
+     * @var string|array
+     */
     public string|array $replyKeyboard = null;
+
+    /**
+     * Holds the json of inline keyboard
+     *
+     * @var string|array
+     */
     public string|array $inlineKeyboard = null;
+
+    /**
+     * Holds the form data
+     *
+     * @var string
+     */
     public string $form = null;
 
 }
