e5d9a556b7107d2d6423009b81083bbf277576ea
Eraj <eraj@email.ir>
2023-12-08 08:01:46 +0330

    writing some namespaces

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -46,6 +46,8 @@ class Autoload extends AutoloadConfig
         'Libraries' => APPPATH . 'Libraries' ,
         'Libraries\GapAPI' => APPPATH . 'Libraries/GapAPI' ,
         'Libraries\GapAPI\Handlers' => APPPATH . 'Libraries/GapAPI/Handlers' ,
+        'Libraries\GapAPI\Send' => APPPATH . 'Libraries/GapAPI/Send' ,
+        'Libraries\GapAPI\Send\Handlers' => APPPATH . 'Libraries/GapAPI/Send/Handlers' ,
         'Libraries\TestAPI' => APPPATH . 'Libraries/TestAPI' ,
     ];
 
