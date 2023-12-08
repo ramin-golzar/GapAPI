2940b92223d1b268ffd82016e1f6b34ee702ec53
Eraj <eraj@email.ir>
2023-12-08 07:07:44 +0330

    Writing all constant in the URLs.php

--- app/Libraries/GapAPI/Send/Handlers/URLs.php
+++ app/Libraries/GapAPI/Send/Handlers/URLs.php
@@ -1,7 +1,23 @@
 <?php
 namespace App\Libraries\GapAPI\Send\Handlers;
 
+/**
+ * This class contains URLs for send
+ */
 class URLs
 {
+
+    public const BASE_URL = 'https://api.gap.im/';
+    public const SEND_MESSAGE = 'sendMessage';
+    public const UPLOAD = 'upload';
+    public const EDIT_MESSAGE = 'editMessage';
+    public const SEND_ACTION = 'sendAction';
+    public const DELETE_MESSAGE = 'deleteMessage';
+    public const ANSWER_CALLBACK = 'answerCallback';
+    public const PAYMENT_INQUIRY = 'payment/inquiry';
+    public const PAYMENT_VERIFY = 'payment/verify';
+    public const INVOICE_INQUIRY = 'invoice/inquiry';
+    public const INVOICE_VERIFY = 'invoice/verify';
+    public const INVOICE = 'invoice';
 
 }
