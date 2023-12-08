32690d81ca933a74e4b7f8ecc1f87e33bee84420
Eraj <eraj@email.ir>
2023-12-08 04:34:58 +0330

    remove code in the GapAPI.php

--- app/Libraries/GapAPI/GapAPI.php
+++ app/Libraries/GapAPI/GapAPI.php
@@ -15,54 +15,6 @@ use App\Libraries\GapAPI\Messages\Send;
 class GapAPI
 {
 
-    private object $sendParams = null;
-
-    public function send_join_msg (SendParams $params): void {
-
-    }
-
-    public function send_message (string $token): void {
-        $this->sendParams = new SendParams();
-
-        $send = new Send ($this->sendParams , $token);
-    }
-
-    public function set_chat_id (string $chatId = null): object {
-        $this->sendParams->chatId = $chatId;
-
-        return $this;
-    }
-
-    public function set_type (string $type = 'text'): object {
-        $this->sendParams->type = $type;
-
-        return $this;
-    }
-
-    public function set_data (string $data): object {
-        $this->sendParams->data = $data;
-
-        return $this;
-    }
-
-    public function set_reply_keyboard (string|array $keyboard = null): object {
-        $this->sendParams->replyKeyboard = $keyboard;
-
-        return $this;
-    }
-
-    public function set_inline_keyboard (string|array $keyboard = null): object {
-        $this->sendParams->inlineKeyboard = $keyboard;
-
-        return $this;
-    }
-
-    public function set_form (string|array $form = null): object {
-        $this->sendParams->form = $form;
-
-        return $this;
-    }
-
 }
 
 // this code fot test the connection
