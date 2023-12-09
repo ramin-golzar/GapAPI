aec56474da4fca6df2f2ddac6859dad7587ad2ce
Eraj <eraj@email.ir>
2023-12-09 07:31:29 +0330

    I create the Multipart.php class & wrote required properties in it

---
+++ app/Libraries/GapAPI/Handlers/Multipart.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Libraries\GapAPI\Handlers;
+
+class Multipart
+{
+
+    public string $chat_id = null;
+    public object $image = null;
+    public object $video = null;
+    public object $audio = null;
+    public object $voice = null;
+    public object $file = null;
+    public string $desc = null;
+
+}
