7d1cb0c8952c1f8e91899d84ba89be1392904d6f
Eraj <eraj@email.ir>
2023-12-09 07:40:13 +0330

    I wrotes comments for all properties of Multipart.php class

--- app/Libraries/GapAPI/Handlers/Multipart.php
+++ app/Libraries/GapAPI/Handlers/Multipart.php
@@ -4,12 +4,53 @@ namespace App\Libraries\GapAPI\Handlers;
 class Multipart
 {
 
+    /**
+     * Holds the chatId params
+     *
+     * @var string
+     */
     public string $chat_id = null;
+
+    /**
+     * This is CurlFile object for send image
+     *
+     * @var object
+     */
     public object $image = null;
+
+    /**
+     * This is CurlFile object for send video
+     *
+     * @var object
+     */
     public object $video = null;
+
+    /**
+     * This is CurlFile object for send audio
+     *
+     * @var object
+     */
     public object $audio = null;
+
+    /**
+     * This is CurlFile object for send voice
+     *
+     * @var object
+     */
     public object $voice = null;
+
+    /**
+     * This is CurlFile object for send file
+     *
+     * @var object
+     */
     public object $file = null;
+
+    /**
+     * Holds a description for a file
+     *
+     * @var string
+     */
     public string $desc = null;
 
 }
