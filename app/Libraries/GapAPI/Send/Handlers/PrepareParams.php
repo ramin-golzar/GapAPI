4a518cd2b0e37709f278aa8fd50056d300382c50
Eraj <eraj@email.ir>
2023-12-09 05:03:41 +0330

    Write the run() , ignoring_null_params() and set_chat_id() function in the PrepareParams.php class

--- app/Libraries/GapAPI/Send/Handlers/PrepareParams.php
+++ app/Libraries/GapAPI/Send/Handlers/PrepareParams.php
@@ -2,13 +2,33 @@
 namespace App\Libraries\GapAPI\Send\Handlers;
 
 use App\Libraries\GapAPI\Handlers\Params;
+use App\Libraries\GapAPI\Handlers\Codes;
 
 class PrepareParams
 {
 
+    /**
+     * Start preparing the parameters
+     *
+     * @return array|null
+     */
     public function run (): array|null {
         $params = new Params();
 
+        $this->set_chat_id ($params);
+
+        $result = $this->ignoring_null_params ($params);
+
+        return $result;
+    }
+
+    /**
+     * Ignoring the null parameters
+     *
+     * @param Params $params
+     * @return array|null
+     */
+    private function ignoring_null_params (Params &$params): array|null {
         $result = [];
 
         foreach ($params as $k => $v) {
@@ -17,7 +37,19 @@ class PrepareParams
             }
         }
 
-        return $result ?: null;
+        return $result;
+    }
+
+    /**
+     * Setting a value for chat_id parameter
+     *
+     * @param Params $params
+     * @return string|null
+     */
+    private function set_chat_id (Params &$params): string|null {
+        $codes = new Codes();
+
+        return $params->chat_id ?? $codes->get_chat_id ();
     }
 
 }
