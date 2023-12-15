b3348b3ae7ff00b77e18429059033e5042ce25c3
Eraj <eraj@email.ir>
2023-12-15 09:11:55 +0330

    Created the ReplyKeyboard.php & write a example in it

---
+++ app/Libraries/GapAPI/Templates/ReplyKeyboard.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Libraries\GapAPI\Templates;
+
+class ReplyKeyboard
+{
+
+    public array $example = [
+        [
+            ['Yes' => 'Yes' ,] ,
+            ['No' => 'No' ,]
+        ] ,
+        [
+            ['Cancel' => 'Cancel' ,]
+        ]
+    ];
+
+}
