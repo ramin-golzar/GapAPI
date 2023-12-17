e8741c6648d07acda872438927bdc1c86ad99e99
Eraj <eraj@email.ir>
2023-12-17 04:31:19 +0330

    I commented the all example of templates

--- app/Libraries/GapAPI/Templates/Form.php
+++ app/Libraries/GapAPI/Templates/Form.php
@@ -8,30 +8,35 @@ class Form
      * text , textarea , radio , checkbox , inbuilt , select , submit
      */
 
-    public array $example = [
-        [
-            'type' => 'text' ,
-            'name' => 'name' ,
-            'label' => 'Your Name' ,
-        ] ,
-        [
-            'type' => 'radio' ,
-            'name' => 'radio' ,
-            'options' => [
-                ['man' => 'Man' ,] ,
-                ['women' => 'Women' ,]
-            ] ,
-            'label' => 'Jender' ,
-        ] ,
-        [
-            'type' => 'select' ,
-            'name' => 'select_color' ,
-            'label' => 'Select a color' ,
-            'options' => [
-                ['red' => 'Red' ,] ,
-                ['green' => 'Green' ,]
-            ] ,
-        ] ,
-    ];
+
+    // This is a example for form template
+
+    /*
+      public array $example = [
+      [
+      'type' => 'text' ,
+      'name' => 'name' ,
+      'label' => 'Your Name' ,
+      ] ,
+      [
+      'type' => 'radio' ,
+      'name' => 'radio' ,
+      'options' => [
+      ['man' => 'Man' ,] ,
+      ['women' => 'Women' ,]
+      ] ,
+      'label' => 'Jender' ,
+      ] ,
+      [
+      'type' => 'select' ,
+      'name' => 'select_color' ,
+      'label' => 'Select a color' ,
+      'options' => [
+      ['red' => 'Red' ,] ,
+      ['green' => 'Green' ,]
+      ] ,
+      ] ,
+      ];
+     */
 
 }
--- app/Libraries/GapAPI/Templates/InlineKeyboard.php
+++ app/Libraries/GapAPI/Templates/InlineKeyboard.php
@@ -3,24 +3,34 @@ namespace App\Libraries\GapAPI\Templates;
 
 class InlineKeyboard
 {
+    /*
+     * Allowed keys for inline keyboard:
+     * text , cb_data , url , open_in , amount , currency ,
+     * ref_id , desc
+     */
 
-    public array $example = [
-        [
-            [
-                'text' => 'File' ,
-                'cb_data' => 'get film' ,
-            ] ,
-            [
-                'text' => 'Photo' ,
-                'cb_data' => 'get photo' ,
-            ]
-        ] ,
-        [
-            [
-                'text' => 'Cancel' ,
-                'cb_data' => 'cancel' ,
-            ]
-        ]
-    ];
+
+    // This is a example for inline keyboard template
+
+    /*
+      public array $example = [
+      [
+      [
+      'text' => 'File' ,
+      'cb_data' => 'get film' ,
+      ] ,
+      [
+      'text' => 'Photo' ,
+      'cb_data' => 'get photo' ,
+      ]
+      ] ,
+      [
+      [
+      'text' => 'Cancel' ,
+      'cb_data' => 'cancel' ,
+      ]
+      ]
+      ];
+     */
 
 }
