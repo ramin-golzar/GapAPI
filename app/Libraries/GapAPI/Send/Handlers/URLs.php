e760fa872ed1c60bae15725b2154561be0c8e269
Eraj <eraj@email.ir>
2023-12-18 05:02:22 +0330

    I converted the URLs class to a enum type

--- app/Libraries/GapAPI/Send/Handlers/URLs.php
+++ app/Libraries/GapAPI/Send/Handlers/URLs.php
@@ -4,20 +4,20 @@ namespace App\Libraries\GapAPI\Send\Handlers;
 /**
  * This class contains URLs for send
  */
-class URLs
+enum URLs: string
 {
 
-    public const BASE_URL = 'https://api.gap.im/';
-    public const SEND_MESSAGE = 'sendMessage';
-    public const UPLOAD = 'upload';
-    public const EDIT_MESSAGE = 'editMessage';
-    public const SEND_ACTION = 'sendAction';
-    public const DELETE_MESSAGE = 'deleteMessage';
-    public const ANSWER_CALLBACK = 'answerCallback';
-    public const PAYMENT_INQUIRY = 'payment/inquiry';
-    public const PAYMENT_VERIFY = 'payment/verify';
-    public const INVOICE_INQUIRY = 'invoice/inquiry';
-    public const INVOICE_VERIFY = 'invoice/verify';
-    public const INVOICE = 'invoice';
+    case base_url = 'https://api.gap.im/';
+    case send_message = 'sendMessage';
+    case upload = 'upload';
+    case edit_message = 'editMessage';
+    case send_action = 'sendAction';
+    case delete_message = 'deleteMessage';
+    case answer_callback = 'answerCallback';
+    case payment_inquiry = 'payment/inquiry';
+    case payment_verify = 'payment/verify';
+    case invoice_inquiry = 'invoice/inquiry';
+    case invoice_verify = 'invoice/verify';
+    case invoice = 'invoice';
 
 }
