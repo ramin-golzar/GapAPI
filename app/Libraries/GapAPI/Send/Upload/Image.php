1f80059171cd2caeb7e192eb9b62e206edc01c2a
Eraj <eraj@email.ir>
2023-12-29 18:06:58 +0330

    I created the Image.php & wrote code of it

---
+++ app/Libraries/GapAPI/Send/Upload/Image.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Libraries\GapAPI\Send;
+
+use App\Libraries\GapAPI\Handlers\FormParams;
+use App\Libraries\GapAPI\Handlers\Multipart;
+use App\Libraries\GapAPI\Send\Handlers\URLs;
+use App\Libraries\GapAPI\Send\Handlers\Types;
+
+class Image extends BaseSend
+{
+
+    public function __construct (object &$client , ?FormParams &$formParams , ?Multipart &$multipart = null) {
+        parent::__construct ($client , null , $multipart);
+
+        $this->set_type (Types::image);
+
+        $this->set_method (URLs::upload);
+    }
+
+}
