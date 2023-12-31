b17583e888155f2745434ffd339380a527711bdd
Eraj <eraj@email.ir>
2023-12-31 20:58:33 +0330

    I wrote the get_from function in the Receive.php

--- app/Libraries/GapAPI/Receive/Receive.php
+++ app/Libraries/GapAPI/Receive/Receive.php
@@ -4,14 +4,24 @@ namespace App\Libraries\GapAPI\Receive;
 class Receive
 {
 
-    private object $request;
+    private object $post;
 
     public function __construct (object $request) {
-        $this->request = (object) $request->getPost ();
+        $this->post = (object) $request->getPost ();
     }
 
     public function get_chat_id (): string|false {
-        return $this->request->chat_id ?: false;
+        return $this->post->chat_id ?: false;
+    }
+
+    public function get_from (string $fromKey = ''): array|string|false {
+        if (!$this->post->from) {
+            return false;
+        }
+
+        $fromDecoded = json_decode ($this->post->from);
+
+        return $fromKey ? $fromDecoded [$fromKey] : $fromDecoded;
     }
 
 }
