0a62994f33489a928c8c80b7fb7f8d71dde17037
Eraj <eraj@email.ir>
2024-01-07 20:07:54 +0330

    I wrote some comments of Receive.php functions

--- app/Libraries/GapAPI/Receive.php
+++ app/Libraries/GapAPI/Receive.php
@@ -47,20 +47,32 @@ trait Receive
      */
     public function get_from (?string $fromKey = null , bool $decoding = true): array|string|false {
         if (isset ($this->post->from)) {
-            return $this->post_analysis ('from' , $decoding , $fromKey);
+            return $this->get_from_post ('from' , $decoding , $fromKey);
         }
 
         return false;
     }
 
+    /**
+     * To get the text content
+     *
+     * @return string|false
+     */
     public function get_text (): string|false {
         if ($this->exist_type (ReceiveTypes::text)) {
-            return $this->post_analysis ('data');
+            return $this->get_from_post ('data');
         }
 
         return false;
     }
 
+    /**
+     * To check exist a specific value of
+     * type key into the post method
+     *
+     * @param ReceiveTypes $type
+     * @return bool
+     */
     private function exist_type (ReceiveTypes $type): bool {
         if (isset ($this->post->type) && $this->post->type == $type->name) {
             return true;
@@ -69,7 +81,16 @@ trait Receive
         return false;
     }
 
-    private function post_analysis (string $postKey , bool &$decoding = false , ?string $postKeyKey = null): string|array|false {
+    /**
+     * To get a value of a specific key
+     * from the post method
+     *
+     * @param string $postKey
+     * @param bool $decoding
+     * @param string|null $postKeyKey
+     * @return string|array|false
+     */
+    private function get_from_post (string $postKey , bool &$decoding = false , ?string $postKeyKey = null): string|array|false {
         if (!isset ($this->post->$postKey)) {
             return false;
         } elseif (isset ($this->post->$postKey) && !$decoding) {
