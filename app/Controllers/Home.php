a4a0404843930eb0100da393deb77c461352cd5e
Eraj <eraj@email.ir>
2024-01-16 09:56:12 +0330

    I wrote some comments in the home controller

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,7 +4,18 @@ namespace App\Controllers;
 class Home extends BaseController
 {
 
+    /**
+     * To holds token of bot
+     *
+     * @var string
+     */
     private string $token = '18b34dbfab054137d021173fbcc12fc0ee01bca35c8a2d52b566585b1ff71496';
+
+    /**
+     * To holds the user chat id
+     *
+     * @var string
+     */
     private string $chatId = '339322905';
 
     public function index () {
