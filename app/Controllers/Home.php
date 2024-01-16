597ff1ed2524715b528a491cf254c8b8499b8555
Eraj <eraj@email.ir>
2024-01-16 10:54:12 +0330

    I wrote code to list,register,menu in the home controller

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -18,8 +18,76 @@ class Home extends BaseController
      */
     private string $chatId = '339322905';
 
+    /**
+     * Gap API
+     *
+     * @var object
+     */
+    private object $gap;
+
+    /**
+     * To holds keyboards
+     *
+     * @var array
+     */
+    private array $keyboards = [
+        'main' => [[['list' => 'list']] , [['register' => 'register']]] ,
+        'menu' => [[['menu' => 'menu' ,]]] ,
+        'list' => [
+            [
+                ['text' => 'image' , 'cb_data' => 'image']
+            ] ,
+            [
+                ['text' => 'video' , 'cb_data' => 'video']
+            ] ,
+            [
+                ['text' => 'Audio' , 'cb_data' => 'Audio']
+            ] ,
+            [
+                ['text' => 'voice' , 'cb_data' => 'voice']
+            ] ,
+            [
+                ['text' => 'file' , 'cb_data' => 'file']
+            ]
+        ] ,
+        'register' => [
+            [
+                'type' => 'text' ,
+                'name' => 'first_name' ,
+                'label' => 'first name:' ,
+            ] ,
+            [
+                'type' => 'text' ,
+                'name' => 'last_name' ,
+                'label' => 'last name:' ,
+            ]
+        ] ,
+    ];
+
     public function index () {
-        $gap = new \App\Libraries\GapAPI\GapAPI ($this->token , $this->request);
+        $this->gap = new \App\Libraries\GapAPI\GapAPI ($this->token , $this->request);
+
+        $this->process_input ();
+    }
+
+    private function process_input (): void {
+        $text = $this->gap->get_text ();
+
+        switch ($text) {
+            case 'list':
+                $this->gap->set_inline_keyboard ($this->keyboards['list'])
+                    ->set_reply_keyboard ($this->keyboards ['menu'])
+                    ->send_text ('Please select one glass keyboard');
+                break;
+            case 'register':
+                $this->gap->set_form ($this->keyboards['register'])
+                    ->set_reply_keyboard ($this->keyboards['menu'])
+                    ->send_text ('Please fill this form');
+            case 'menu':
+            default :
+                $this->gap->set_reply_keyboard ($this->keyboards['main'])
+                    ->send_text ('Please select one of buttons below');
+        }
     }
 
 }
