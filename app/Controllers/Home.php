502c6fe536a0036faf1eefa36a29e56feb5728c2
Eraj <eraj@email.ir>
2024-01-19 17:11:44 +0330

    I deleted the token & chatId

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -9,14 +9,14 @@ class Home extends BaseController
      *
      * @var string
      */
-    private string $token = '18b34dbfab054137d021173fbcc12fc0ee01bca35c8a2d52b566585b1ff71496';
+    private string $token = '';
 
     /**
      * To holds the user chat id
      *
      * @var string
      */
-    private string $chatId = '339322905';
+    private string $chatId = '';
 
     /**
      * Gap API
